5020dcb9d79be9fb7d0b9c03d9db9f9a6f3f4041
vickysharma29 <137756045+vickysharma29@users.noreply.github.com>
2023-06-26 13:14:49 +0530

    Update streamlit_app.py
    show only selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ my_fruit_list = pandas.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.c
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # lets put a pick list so that they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruit_to_show = my_fruit_list.loc[fruits_selected]
 
 # Disply the table on the page
 streamlit.dataframe(my_fruit_list)
